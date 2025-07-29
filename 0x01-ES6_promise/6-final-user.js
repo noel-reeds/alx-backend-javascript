1895c372b542979aa4d9e5ca40f20c84be8233fb
altoids <noelreeds01@gmail.com>
2025-07-29 17:11:53 +0300

    sets up a user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const allSettled = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return allSettled.then((resolveValues) => console.log(resolveValues));
+  return allSettled.then((resolveValues) => resolveValues);
 }
