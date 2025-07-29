10a245d12356311417ad940d63f51dc95b59bd07
altoids <noelreeds01@gmail.com>
2025-07-29 16:20:22 +0300

    returns resolved promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import * as utils from './utils';
 
 export default function handleProfileSignup() {
   const promise = Promise.all([utils.uploadPhoto(), utils.createUser()]);
-  promise.then((attrs) => {
+  return promise.then((attrs) => {
     console.log(`${attrs[0].body} ${attrs[1].firstName} ${attrs[1].lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }
