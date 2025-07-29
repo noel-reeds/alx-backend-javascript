2c5d278b20a20417400d0684bac7e203c3504bde
altoids <noelreeds01@gmail.com>
2025-07-29 18:01:17 +0300

    attempts to drop stack trace on promise.reject

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const allSettled = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return allSettled.then((resolveValues) => resolveValues);
+  const promise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promise.then((arr) => arr);
 }
