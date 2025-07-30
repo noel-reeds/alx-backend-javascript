5ba9f797180f53128b32bcc175d8e4b915c17e6d
altoids <noelreeds01@gmail.com>
2025-07-30 15:30:57 +0300

    captures a thrown err

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  const res = mathFunction();
+  if (isNaN(res)) {
+    queue.push(`Error: ${res.message}`, 'Guardrail was processed');
+  } else {
+    queue.push(res, 'Guardrail was processed');
+  }
+  return queue;
+}
