7f7d76961d22e356721da5aa0f0acf6cee0d3736
altoids <noelreeds01@gmail.com>
2025-07-30 16:08:49 +0300

    modifies thrown error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   const res = mathFunction();
-  if (Number.isNaN(res)) {
-    queue.push(`Error: ${res.message}`, 'Guardrail was processed');
-  } else {
+  if (Number.isFinite(res)) {
     queue.push(res, 'Guardrail was processed');
+  } else {
+    queue.push(`${res.name}: ${res.message}`, 'Guardrail was processed');
   }
   return queue;
 }
