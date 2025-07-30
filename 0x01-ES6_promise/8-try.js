87a82312b84eeb2af99e1aa1399e1add7ddafecc
altoids <noelreeds01@gmail.com>
2025-07-30 15:36:29 +0300

    logs correct stacktrace of thrown error

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    const res = numerator / denominator;
-    if (Number.isFinite(res)) {
-      return res;
-    }
-    throw Error('cannot divide by 0');
-  } catch (err) {
-    return err;
+  const res = numerator / denominator;
+  if (Number.isFinite(res)) {
+    return res;
   }
+  throw Error('cannot divide by 0');
 }
