192d8f2a16c5a1ed88096ddfe875e5fabd154331
altoids <noelreeds01@gmail.com>
2025-08-04 21:00:07 +0300

    fixes lint errors

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,7 @@
 export default function getStudentIdsSum(studentsArray) {
   const studentsIds = studentsArray.map((student) => student.id);
   const sumOfIds = studentsIds.reduce(
-    (acc, curr) => acc + curr);
+    (acc, curr) => acc + curr,
+  );
   return sumOfIds;
 }
