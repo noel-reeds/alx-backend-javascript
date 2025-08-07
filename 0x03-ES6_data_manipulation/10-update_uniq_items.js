b13ab78964e39b2d04eab06cec6b028388812d3f
altoids <noelreeds01@gmail.com>
2025-08-07 13:20:06 +0300

    checks for a valid map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,12 @@
 export default function updateUniqueItems(map) {
-  for (const [k, v] of map[Symbol.iterator]()) {
-    if (v === 1) {
-      map.set(k, 100);
+  if (map instanceof Map) {
+    for (const [k, v] of map[Symbol.iterator]()) {
+      if (v === 1) {
+        map.set(k, 100);
+      }
     }
+    return map;
+  } else {
+    throw Error('Cannot process');
   }
-  return map
 }
