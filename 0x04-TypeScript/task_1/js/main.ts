693ce47a1f76a250eccb4a60e8575505f34e8f13
altoids <noelreeds01@gmail.com>
2025-08-28 08:42:35 +0300

    returns the first letter of the firstName and the full lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+
+function printTeacher(teacher: printTeacherFunction) {
+  return teacher.firstName[0] + `. ${teacher.lastName}`;
+};
